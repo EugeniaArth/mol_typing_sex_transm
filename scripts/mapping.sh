19b2073bc119cc0019a34db490766f061fb1fa8e
Eugenia Nikonorova <59174681+EugeniaArth@users.noreply.github.com>
2024-08-19 16:41:32 +0300

    Update mapping.sh
    Work for multiply files in folders Country with subfolder Trimmed, output is redirected to Mapped folder for the next step of analysis

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -1,19 +1,51 @@
-#!bin/bash
+#!/bin/bash
+#for multiply countries after trimming 
 
-#bowtie2.sh
 
-for f in ~/Genomes_mol_typing/Chlamydiales/SRA/Fastq/Italy/*_1.fastq.gz # for each sample
-do
-  i=${f%%_1.fastq.gz}
-  bowtie2 -p 8  -1 ${i}_1.fastq.gz  -2 ${i}_2.fastq.gz -x ~/Genomes_mol_typing/Chlamydiales/reference/Chl.trach_index -S ${i}.sam
-echo ${i} mapping is finished
-  samtools view -bS -F 0x4 ${i}.sam -o ${i}.bam
-echo ${i}.bam is recieved
-  samtools sort ${i}.bam -o ${i}.sorted.bam
-echo ${i}.bam is sorted
-#  mv ${i}.sorted.bam ~/Genomes_mol_typing/Chlamydiales/GATK4/mapped ~/Genomes_mol_typing/Chlamydiales/GATK4/calling/Italy
-echo ${i} was moved to GATK4/calling folder
-  #rm ${i}.bam
-  #rm ${i}.sam
-  echo ${i} .bam and .sam were removed
+# Export the path to BWA
+export PATH="/Users/eugenianikonorova/bwa:$PATH"
+
+# Path to the reference genome index
+REFERENCE_INDEX="/Users/eugenianikonorova/Genomes_mol_typing/Chlamydiales/reference/DUW_3CX"
+
+# Base directories
+SRA_BASE_DIR="/Users/eugenianikonorova/Genomes_mol_typing/Chlamydiales/SRA"
+MAPPED_BASE_DIR="/Users/eugenianikonorova/Genomes_mol_typing/Chlamydiales/GATK4/mapped"
+
+# Iterate over each country directory
+for COUNTRY_DIR in $SRA_BASE_DIR/*; do
+    # Ensure it is a directory
+    if [ -d "$COUNTRY_DIR" ]; then
+        COUNTRY_NAME=$(basename "$COUNTRY_DIR")
+
+        # Create the mapped directory for the country if it doesn't exist
+        MAPPED_COUNTRY_DIR="$MAPPED_BASE_DIR/$COUNTRY_NAME"
+        mkdir -p "$MAPPED_COUNTRY_DIR"
+
+        # Iterate over each fastq.gz file pair in the Trimmed subfolder
+        for F in "$COUNTRY_DIR/Trimmed/"*_1_trimmed.fastq.gz; do
+            I=${F%%_1_trimmed.fastq.gz}
+
+            # Perform bwa mem alignment
+            bwa mem -t 8 "$REFERENCE_INDEX" "${I}_1_trimmed.fastq.gz" "${I}_2_trimmed.fastq.gz" > "${I}.sam"
+            echo "${I} mapping is finished"
+
+            # Convert SAM to BAM
+            samtools view -bS -F 0x4 "${I}.sam" -o "${I}.bam"
+            echo "${I}.bam is received"
+
+            # Sort BAM file
+            samtools sort "${I}.bam" -o "${I}.sorted.bam"
+            echo "${I}.bam is sorted"
+
+            # Move the BAM files to the mapped directory
+            mv "${I}.bam" "${MAPPED_COUNTRY_DIR}/"
+            mv "${I}.sorted.bam" "${MAPPED_COUNTRY_DIR}/"
+            echo "${I} was moved to $MAPPED_COUNTRY_DIR"
+
+            # Clean up SAM file
+            rm "${I}.sam"
+            echo "${I}.sam was removed"
+        done
+    fi
 done
